250a0e0d013b0aa3d9ad87a51b90ff128d543aa2
Prathyush <kingspprathyush@gmail.com>
2015-07-11 15:06:48 +0530

    Create online-registration.php

---
+++ online-registration.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<title>Free Online Test PHP Script</title>
+	<style>
+		body{
+			background: #f4702f;
+			font-family:"Century Gothic";
+			font-size:13px;
+		}
+		.que-rows {}
+		.que-rows .que-row {}
+		.que-rows .que-row .que-row-options {
+			display: block;
+			clear: both;
+		}
+		.que-rows .que-row .que-row-options input {
+			float: left;
+		}
+		.que-rows .que-row .que-row-options label {}		
+		.goahead {}
+	</style>
+</head>
+
+<body> 
+
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div align="center">
+<div style="width:450px; background-color: #FFFFFF; padding:10px 13px;  border-radius:8px; -webkit-border-radius:8px; -moz-border-radius:8px; -o-border-radius:8px;">
+
+<p align="center">
+
+
+
+<div>&nbsp;</div>
+
+<strong><u>Test Registration</u></strong>
+
+<div>&nbsp;</div>
+
+<form accept-charset="UTF-8" action="" class="infusion-form" method="POST">
+    <input name="inf_form_xid" type="hidden" />
+    <input name="inf_form_name" type="hidden" value="Online Test Script Registration" />
+    <input name="inf_version" type="hidden" value="" />
+
+<table width="95%" cellpadding="5">
+  <tr>
+    <td align="right"><label for="inf_field_FirstName">Your First Name *</label></td>
+    <td align="left"><input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="text" /></td>
+  </tr>
+  <tr>
+    <td align="right"><label for="inf_field_LastName">Your Last Name *</label></td>
+    <td align="left"><input class="infusion-field-input-container" id="inf_field_LastName" name="inf_field_LastName" type="text" /></td>
+  </tr>
+  <tr>
+    <td align="right"><label for="inf_field_Email">Your Email Address *</label></td>
+    <td align="left"><input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="text" /></td>
+  </tr>
+    <tr>
+    <td align="right">&nbsp;</td>
+    <td align="left"><input type="submit" value="Take Online Test" /></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="left"><strong>Important!</strong> Be certain to use your own name & e-mail address when completing the registration form above. Your name will be printed on your certificate, and your certificate will be forwarded to your email address (optional). <br />
+      <br />
+	<strong>Privacy Notice:</strong> Your information will not be shared.</td>
+  </tr>
+</table>
+
+	<div>&nbsp;</div>
+</form>
+
+</p>
+
+</div>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/fs.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
